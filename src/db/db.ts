cdfdc8171aae69e938d6c76d26c88103d71c9338
KirillZverev <zverev04@gmail.com>
2026-09-10 21:53:36 +0300

    test commit

--- src/db/db.ts
+++ src/db/db.ts
@@ -15,30 +15,27 @@ export type VideoType = {
 
 export type DBType = { videos: VideoType[] };
 
-// export const db: DBType = {
-//     videos: [
-//         {
-//             id: 1,
-//             title: "super_video",
-//             author: "student",
-//             canBeDownloaded: false,
-//             minAgeRestriction: 12,
-//             createdAt: "2026-09-08T12:06:53.184Z",
-//             publicationDate: "2026-10-08T12:06:53.184Z",
-//             availableResolutions: ["P240", "P360", "P480", "P720", "P1080"]
-//         },
-//         {
-//             id: 2,
-//             title: "just_video",
-//             author: "coach",
-//             canBeDownloaded: true,
-//             minAgeRestriction: 18,
-//             createdAt: "2026-09-15T12:06:53.184Z",
-//             publicationDate: "2026-10-15T12:06:53.184Z",
-//             availableResolutions: ["P240", "P360", "P480", "P720"]
-//         }
-//     ]
-// };
 export const db: DBType = {
-    videos: []
+    videos: [
+        {
+            id: 1,
+            title: "super_video",
+            author: "student",
+            canBeDownloaded: false,
+            minAgeRestriction: 12,
+            createdAt: "2026-09-08T12:06:53.184Z",
+            publicationDate: "2026-10-08T12:06:53.184Z",
+            availableResolutions: ["P240", "P360", "P480", "P720", "P1080"]
+        },
+        {
+            id: 2,
+            title: "just_video",
+            author: "coach",
+            canBeDownloaded: true,
+            minAgeRestriction: 18,
+            createdAt: "2026-09-15T12:06:53.184Z",
+            publicationDate: "2026-10-15T12:06:53.184Z",
+            availableResolutions: ["P240", "P360", "P480", "P720"]
+        }
+    ]
 };
